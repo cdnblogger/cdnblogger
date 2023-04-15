d6e338793d91a5bc662d7ffe6064d01f01c998c3
cdnblogger <81550362+cdnblogger@users.noreply.github.com>
2023-04-16 01:54:41 +0700

    Add files via upload

---
+++ icon-ki/main/js/rcki.js
@@ -0,0 +1,10 @@
+    var
+ numComments  = 10,
+ showAvatar  = true,
+ avatarSize  = 45,
+ roundAvatar = true,
+ characters  = 80,
+ defaultAvatar  = "http://www.gravatar.com/avatar/?d=mm",
+ hideCredits = true;
+ maxfeeds=50,
+ adminBlog='Kang';
