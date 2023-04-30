7ebedb970482a9c157ac8899c7d7cd39cf434700
cdnblogger <81550362+cdnblogger@users.noreply.github.com>
2023-04-30 20:52:35 +0700

    Create corkismet.js

---
+++ icon-ki/js/corkismet.js
@@ -0,0 +1,21 @@
+var multiFeed = {
+    feedsUri: [
+        {
+            name: "Corkismet",
+            url: "https://corkismet.blogspot.com/",
+            tag: ""
+        }
+    ],
+    numPost: 4,
+    showThumbnail: true,
+    showSummary: false,
+    summaryLength: 80,
+    titleLength: "auto",
+    thumbSize: 72,
+    newTabLink: true,
+    containerId: "feed-list-container",
+    readMore: {
+        text: "Selengkapnya",
+        endParam: "?max-results=8"
+    }
+};
